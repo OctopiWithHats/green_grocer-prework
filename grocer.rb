e0d64764f834f3b0affe971adc39bc976a5a590f
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-23 15:38:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -23,7 +23,7 @@ def apply_coupons(cart, coupons)
   cart.each do |food, info|
     coupons.each do |coupon|
       if food == coupon[:item] && info[:count] >= coupon[:num]
-        
+        info[:count]
 end
 
 def apply_clearance(cart)
